d210c8ff0822ca273eb0a917b9aa38027d0cd448
Yakup Emre Şahin <67762641+yakupemresahin@users.noreply.github.com>
2024-01-18 19:00:36 +0100

    Update resume-data.tsx

--- src/data/resume-data.tsx
+++ src/data/resume-data.tsx
@@ -20,18 +20,18 @@ import {
 import { GitHubIcon, LinkedInIcon, XIcon } from "@/components/icons";
 
 export const RESUME_DATA = {
-  name: "Bartosz Jarocki",
+  name: "Yakup Emre Şahin",
   initials: "BJ",
-  location: "Wrocław, Poland, CET",
-  locationLink: "https://www.google.com/maps/place/Wrocław",
+  location: "Munich, Germany",
+  locationLink: "https://www.google.com/maps/place/Muenchen",
   about:
-    "Full Stack Engineer focused on building products with extra attention to detail",
+    "Physics Master Student with Artificial Intelligence Minor",
   summary:
     "As a Full Stack Engineer, I have successfully taken multiple products from 0 to 1. I lead teams effectively, ensuring an environment where people can do their best work. Currently, I work mostly with TypeScript, React, Node.js, and GraphQL. I have over 8 years of experience in working remotely with companies all around the world.",
   avatarUrl: "https://avatars.githubusercontent.com/u/1017620?v=4",
   personalWebsiteUrl: "https://jarocki.me",
   contact: {
-    email: "bartosz.jarocki@hey.com",
+    email: "yakup.sahin@campus.lmu.de",
     tel: "+48530213401",
     social: [
       {
